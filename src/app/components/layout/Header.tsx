7a42665eb313d4177c5fcb06f96832f3dcec48db
momiprog <k.aramomi0731@gmail.com>
2026-03-26 01:52:14 +0900

    Headerをpage.tsxにインポート

---
+++ src/app/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+            <h1 className="bg-blue-600 text-3xl text-white py-2 px-3 shadow-md">
+                チャットルーム
+            </h1>
+        </header>
+    );
+}
